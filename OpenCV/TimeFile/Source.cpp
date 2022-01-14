e08d962d05ea5eb96db82efd8a7a06802d22de07
CHVRXN <90219892+CHVRXN@users.noreply.github.com>
2022-01-14 10:03:41 +0300

    Add files via upload

---
+++ OpenCV/TimeFile/Source.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+#include <string>
+#include <cstring>
+using namespace std;
+
+void file(char* dt);
+
+int main() 
+{
+	// current date/time based on current system
+	time_t now = time(0);
+	// convert now to string form
+	char* dt = ctime(&now);
+	cout << "The local date and time is: " << dt << endl;
+	file(dt);
+}
+
+void file(char* dt)
+{
+	ofstream file;
+	file.open("test.txt", ios_base::app);
+	unsigned long milliseconds_since_epoch = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
+	file << milliseconds_since_epoch << " ";
+	file << dt << endl;
+	file.close();
+
+}
